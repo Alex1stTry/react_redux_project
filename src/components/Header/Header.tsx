fc608ab76d24779adcd681bfc70276a307fca9d5
Alex1stTry <malyshevalex1994@gmail.com>
2024-03-17 17:09:00 +0200

    ....

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 
+
+// @ts-ignore
 import css from './Header.module.css'
 
 const Header = () => {
     return (
         <div className={css.Header}>
-            <div >
-                <h2>Movies Database</h2>
-                <div>
+                <div>Movies Database</div>
+                <div className={css.Links}>
                     <NavLink to={''}>Movies</NavLink>
                     <NavLink to={'genres'}>Genres</NavLink>
+                    <NavLink to={'search'}>Search2</NavLink>
                 </div>
-                <h2><span>Theme </span>
-                    ThemeSwitcher</h2>
-            </div>
         </div>
     );
 };
